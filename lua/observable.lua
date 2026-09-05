64a0a0e2268d2b27b5790ae34fd1b17adcf3b50f
Caleb Fringer <cfringer98@gmail.com>
2026-09-05 16:33:47 -0700

    Implement Mapper.

--- lua/observable.lua
+++ lua/observable.lua
@@ -26,28 +26,41 @@ function Observable:pipe(other)
     self:subscribe(function(x)
         other:emit(x)
     end)
+    return other
+end
+
+Mapper = Observable:new()
+
+function Mapper:new(f)
+    -- The object needs to be constructed as an instance
+    -- of Observable first. This is like calling super()
+    local o = Observable.new(self)
+    o.f = f
+    self.__index = self
+    setmetatable(o, self)
+    return o
+end
+
+function Mapper:emit(x)
+    -- Here, self refers to the Mapper instance.
+    local transformed = self.f(x)
+    -- Call Observable.emit() so we don't end up infinitely calling
+    -- Mapper.emit(). Calling self:emit() would pass a Mapper instance to
+    -- emit(), calling this function again recursively.
+    -- If we used Observable:emit(), then the self passed to Observable.emit()
+    -- would be Observable, NOT the current mapper instance.
+    Observable.emit(self, transformed)
 end
 
 -- Examples
 local myObservable = Observable:new()
 local double = function(x) return x * 2 end
-local tapPrint = fp.tap(print)
 
-myObservable:subscribe(
-    fp.pipe(
-        tapPrint
-    )
-)
+myObservable:subscribe(print)
 
-local doubled = Observable:new()
-myObservable:pipe(doubled)
+local doubled = myObservable:pipe(Mapper:new(double))
 
-doubled:subscribe(
-    fp.pipe(
-        double,
-        tapPrint
-    )
-)
+doubled:subscribe(print)
 
 for i = 1, 10 do
     myObservable:emit(i)
